a5b7d19e99bcd19e8a5ec420cca513abd3a39d10
Polsu <polsugi@gmail.com>
2026-04-18 20:10:36 +0200

    added createSocket and createServerAdress in udp.c

--- client-skel/udp.c
+++ client-skel/udp.c
@@ -4,5 +4,21 @@
 
 #include "udp.h"
 
-int sock = socket(AF_INET, SOCK_DGRAM, 0);
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+int createSocket(void)
+{
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    return sock;
+}
+
+struct sockaddr_in createServerAdress(int port, char* server_ip)
+{
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    return server_addr;
+}
