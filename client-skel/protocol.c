05a1466b6395fd58eaff5ec2842b510b1198d7d9
Polsu <polsugi@gmail.com>
2026-04-20 12:40:06 +0200

    created sendRegister method

--- client-skel/protocol.c
+++ client-skel/protocol.c
@@ -10,7 +10,7 @@ vpn_header_t create_register_header(uint16_t my_client_id) {
     vpn_header_t header;
     header.opcode = 0x01;
 
-    // Fill payload with zeros
+    // Fill payload with zeros according to protocol
     for(int i = 0; i < 8; i++) {
         header.payload[i] = 0;
     }
--- client-skel/udp.c
+++ client-skel/udp.c
@@ -8,17 +8,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "protocol.h"
+
 int createSocket(void)
 {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     return sock;
 }
 
-struct sockaddr_in createServerAdress(int port, char* server_ip)
+struct sockaddr_in createServerAddress(int port, char* server_ip)
 {
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
     return server_addr;
+}
+
+void sendResgister(int sock, vpn_header_t header, struct sockaddr_in server_addr)
+{
+    sendto(sock, &header, VPN_HEADER_SIZE, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
 }
